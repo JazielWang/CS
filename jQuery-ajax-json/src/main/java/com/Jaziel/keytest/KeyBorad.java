ae9a1d20532825732d030e2f13a89246dc3b319a
jiewang <17854276279@163.com>
2021-04-11 20:38:28 +0800

    jQuery

---
+++ jQuery-ajax-json/src/main/java/com/Jaziel/keytest/KeyBorad.java
@@ -0,0 +1,16 @@
+package com.Jaziel.keytest;
+
+/**
+ * @author 王杰
+ * @date 2021/4/11 17:23
+ */
+public class KeyBorad {
+    public static void main(String[] args) {
+        System.out.println("hello, java");
+        System.out.println("hello, java");
+        System.out.println("hello, java");
+        System.out.println("hello, java");
+
+    }
+
+}
