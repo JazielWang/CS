843a386b7714e0c280961744cc67fd94de821dd4
jiewang <17854276279@163.com>
2021-04-16 21:50:29 +0800

    ajax

---
+++ jQuery-ajax-json/src/main/java/com/Jaziel/servlet/AjaxServlet.java
@@ -0,0 +1,27 @@
+package com.Jaziel.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 王杰
+ * @date 2021/4/16 20:22
+ */
+@WebServlet("/ajaxServlet")
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        System.out.println(username);
+        resp.getWriter().write(username + "hello");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+}
