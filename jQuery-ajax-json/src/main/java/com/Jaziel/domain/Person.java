24950070e48fe8983c36889b66b74999cf8083fc
jiewang <17854276279@163.com>
2021-04-17 21:42:23 +0800

    json

---
+++ jQuery-ajax-json/src/main/java/com/Jaziel/domain/Person.java
@@ -0,0 +1,44 @@
+package com.Jaziel.domain;
+
+/**
+ * @author 王杰
+ * @date 2021/4/17 21:09
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ jQuery-ajax-json/src/test/java/com/Jaziel/test/JsonTest.java
@@ -0,0 +1,13 @@
+package com.Jaziel.test;
+
+import org.junit.Test;
+
+/**
+ * @author 王杰
+ * @date 2021/4/17 21:11
+ */
+public class JsonTest {
+    public static void main(String[] args) {
+
+    }
+}
