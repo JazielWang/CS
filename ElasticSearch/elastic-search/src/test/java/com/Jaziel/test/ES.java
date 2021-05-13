a3d6ed07ce693abfbd9fa14c0b4f7a576220b3b7
jiewang <17854276279@163.com>
2021-05-13 21:04:56 +0800

    复习

--- ElasticSearch/elastic-search/src/test/java/com/Jaziel/test/ES.java
+++ ElasticSearch/elastic-search/src/test/java/com/Jaziel/test/ES.java
@@ -40,7 +40,7 @@ public class ES {
         Settings build = Settings.builder().put("cluster.name", "my-application").build();
         TransportClient transportClient = new PreBuiltTransportClient(build).addTransportAddress(
                 new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
-        // 创建名称为 index2 的索引
+        // 创建名称为 index3 的索引
         transportClient.admin().indices().prepareCreate("index3").get();
         // 释放资源
         transportClient.close();
@@ -87,7 +87,9 @@ public class ES {
         TransportClient transportClient = new PreBuiltTransportClient(build).addTransportAddress(
                 new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
         // 创建文档内容
-        XContentBuilder builder = XContentFactory.jsonBuilder().startObject().field("id", 1).field("title", "ElasticSearch是一个基于Lucene的搜索服务器").
+        XContentBuilder builder = XContentFactory.jsonBuilder().startObject().
+                field("id", 1).
+                field("title", "ElasticSearch是一个基于Lucene的搜索服务器").
                 field("context", "它提供了一个分布式多用户能力的全文搜索引擎，基于RESTful web接口。Elasticsearch是用Java开发的，并作为Apache许可条款下的开放源码发布，是当前流行的企业级搜索引擎。设计用于云计算中，能够达到实时搜索，稳定，可靠，快速，安装使用方便").
                 endObject();
         // 建立文档对象
